14587acd2a1a794a90aa03e0f2637e79e1334a1e
Sumadevivasukuri63 <sumadevivasukuri1@gmail.com>
2025-06-10 19:43:01 +0530

    python

---
+++ Day12/prac.js
@@ -0,0 +1,62 @@
+//x=-76
+//console.log(x)
+//y=20
+ //console.log(x+y)
+ //console.log(x-y)
+//s="suma"
+//console.log(s)
+//const x=78
+//const y=66
+//console.log(x,y)
+//console.log(x==y)
+let signin=false
+let login=true
+if(login || signin){
+    console.log("hi")
+}
+
+
+let array=[33,18,16,63]
+console.log(array)
+//for(let i=0;i<4;i++){
+    //console.log(array[i]);
+    
+//}
+
+//array.push(6)
+//console.log(array)
+
+//array.shift(6)
+//console.log(array)
+
+let i=4;
+while(i>=0){
+    console.log(array[i])
+    i--;
+
+}
+
+var score=100;
+
+if (score<100) {
+  console.log("runner");
+} 
+else if (score>100) {
+  console.log("Winner");
+}
+ else {
+  console.log("average");
+} 
+
+
+let student= {name:"suma",roll_no:63 };
+for (let elem in student) {
+  console.log(elem,student[elem]);
+}
+
+
+
+
+
+
+
---
+++ Day13/function.js
@@ -0,0 +1,24 @@
+//const hello=function (x,y){
+  //  return x+y;
+//}
+//console.log(hello(2,3));
+
+
+
+const hi=(a,b) =>{
+    return (a+b);
+
+}
+console.log(hi(6,8));
+
+
+
+function suma(x,y){
+    return(x+y)
+}
+console.log(suma(8,9));
+
+function hello(name){
+    return(name)
+}
+console.log(hello("my name is suma"));
