30edf6af09af25d9b5a3ed4c0acccebd8d655f01
Sumadevivasukuri63 <sumadevivasukuri1@gmail.com>
2025-06-12 22:03:08 +0530

    python

---
+++ Day14/obj.js
@@ -0,0 +1,14 @@
+//const student={
+  //  name:"suma",
+    //fun(){
+      //  console.log("hello!")
+
+    //}
+//};
+//student.fun();
+
+
+const suma = document.getElementById('clickHere!');
+suma.addEventListener('click', function () {
+  alert('button pressed');
+});
